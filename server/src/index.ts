75a4a98777765bd359dae9507f9c182a024625e3
Atharva Nidhonkar <atharvanidhonkar10@gmail.com>
2024-07-13 23:41:40 +0530

    done for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,17 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from "./routes/financial-records";
 import cors from "cors";
-
+import path, { dirname } from "path" 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname,"dist")))
+console.log(path.join(__dirname,"dist"));
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"dist/index.html"))
+})
 const mongoURI: string="mongodb+srv://adityanidhonkar2004:yD4cV35UtC94s3gE@moneymate.igx9sct.mongodb.net/"
 mongoose
   .connect(mongoURI)
